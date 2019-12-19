3cec264a875614567b664bea5b5f1d58f171e75f
meholt <meholt90@gmail.com>
2019-12-18 22:27:00 -0800

    implementing ContextProvider in Products component

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 // Components
 import Product from './Product';
 
+// Contexts
+import ProductContext from '../contexts/ProductContext';
+
 const Products = props => {
+
+	const { products, addItem } = useContext(ProductContext);
 	return (
 		<div className="products-container">
 			{props.products.map(product => (
